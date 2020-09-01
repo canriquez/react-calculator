71236a48ce614ce16cc91330b186e8943c195bcf
Carlos Anriquez <canriquez@gmail.com>
2020-09-01 07:26:55 -0300

    refactor App as class component

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,35 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 // eslint-disable-next-line
 import calculate from '../logic/calculate'
-
+/* 
 const App = () => (
 
   <div id="app-container">
     <Display />
     <ButtonPanel />
   </div>
-);
+); */
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      total: null,
+      next: null,
+      operation: null
+    }
+  }
+
+  render() {
+    return (
+      <div id="app-container">
+        <Display />
+        <ButtonPanel />
+      </div>
+    )
+  }
+
+}
+
 
 export default App;
